df7b59b552e7392c2274486ea443bfafba2f1b15
Marat Aubakirov <marataub.511@gmail.com>
2025-06-22 20:55:51 +0300

    допилить бургер

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(function () {
-  $(".gallery__box").slick();
+  $(".gallery__box").slick({
+    prevArrow:
+      '<img class="slider__arrow slider__arrow-left" src="images/arrow-left.svg" alt="arrow-left">',
+    nextArrow:
+      '<img class="slider__arrow slider__arrow-right" src="images/arrow-right.svg" alt="arrow-right">',
+  });
 });
